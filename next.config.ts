288983ae094c81f95f872c15873a34908f165568
Thomas Soares <thomas__soares@hotmail.com>
2025-03-09 23:20:38 -0300

    add pageExtensions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['pages.tsx', 'api.ts', 'api.tsx']
 };
 
 export default nextConfig;
